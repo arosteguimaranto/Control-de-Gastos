0c63d8a2aeabacefb0f92dcf695285ef5df76926
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-31 18:58:04 -0600

    Formatear Números a Dinero

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
-export const ControlPresupuesto = ({presupuesto}) => {
+export const ControlPresupuesto = ({ presupuesto }) => {
+
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
+    }
+
     return (
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
             <div>
@@ -9,7 +17,16 @@ export const ControlPresupuesto = ({presupuesto}) => {
 
             <div className="contenido-presupuesto">
                 <p>
-                    <span> Presupuesto: </span> ${presupuesto} </p>
+                    <span> Presupuesto: </span> {formatearCantidad(presupuesto)}
+                </p>
+
+                <p>
+                    <span> Disponible: </span> {formatearCantidad(0)}
+                </p>
+
+                <p>
+                    <span> Gastado: </span> {formatearCantidad(0)}
+                </p>
             </div>
         </div>
     )
